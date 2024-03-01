9086e20509b746f30d1b42d1922985580c36b387
Saransh05 <123106127+Saransh05@users.noreply.github.com>
2024-03-01 10:18:04 +0530

    Time: 4 ms (55.95%) | Memory: 53.2 MB (5.45%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,24 +1,40 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int i,j,c=0;
-        for(i=0;i<nums.length;i++)
-        {
-            c=1;
-            for(j=i+1;j<nums.length;j++)
+        int c=1,i,res=0;
+        for(i=res+1;i<nums.length;i++)
+        {    
+            if(nums[res]==nums[i])
             {
-                if(nums[j]==nums[i])
-                {
-                    c++;
-                }
-            }
-            if(c>(nums.length/2))
-            {
-                return nums[i];
+                c++;
             }
             else{
-                c=0;
+                c--;
+            }
+            if(c==0)
+            {
+                res=i;
+                c=1;
             }
         }
-        return -1;
+        System.out.println(res);
+       int d= find(res,nums);
+       return d;
+    }
+    public int find(int res,int[] nums)
+    { int c=0;
+        for(int j=0;j<nums.length;j++)
+        {
+            if(nums[j]==nums[res])
+            {
+                c++;
+            }
+        }
+        if(c>nums.length/2)
+        {
+            return nums[res];
+        }
+        else{
+            return -1;
+        }
     }
 }
