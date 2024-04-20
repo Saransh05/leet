700b6b14a9c6a73aa00b7bf02b28bc8f03f15a47
Saransh05 <123106127+Saransh05@users.noreply.github.com>
2024-04-20 18:20:45 +0530

    Time: 10 ms (22.61%) | Memory: 65.2 MB (53.81%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+          Deque<Integer> stack=new ArrayDeque<Integer>();
+        for(ListNode curr=head;curr!=null;curr=curr.next)
+        {
+            stack.push(curr.val);
+        } 
+        for(ListNode curr=head;curr!=null;curr=curr.next)
+        {
+            if(stack.pop()!=curr.val)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
