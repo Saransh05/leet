f22e35263e99bed08cb4a05bba0c8a5b6edee03b
Saransh Kapoor <123106127+Saransh05@users.noreply.github.com>
2024-09-07 16:38:30 +0530

    Time: 2 ms (99.76%) | Memory: 42.5 MB (33.65%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,45 +1,24 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        int i,c1=0,c2=0,c3=0,c4=0;
-        String str1="",str2="",strf="",ret="";
-        for (i=0;i<s.length();i++)
-        {
-            if(s.charAt(i)=='(')
-            {
-                str1=str1+s.charAt(i);
-                c1++;
-                   }
-            else if(s.charAt(i)==')')
-            { 
-                str1=str1+s.charAt(i);
-                c2++;
-            }
-            if(c1==c2)
-            {
-                str2=str2+str1;
-                strf=strf+str2.substring(1,(c1+c2-1));
-                str2="";
-                str1="";
-                c1=0;
-                c2=0;
+        StringBuilder result = new StringBuilder();
+        int openCount = 0; // Keeps track of the number of open parentheses
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                // If openCount > 0, it means we're inside an inner pair, so append the '('
+                if (openCount > 0) {
+                    result.append(c);
+                }
+                openCount++; // Increment the open parentheses count
+            } else {
+                openCount--; // Decrement the open parentheses count
+                // If openCount > 0 after decrementing, it means we're inside an inner pair, so append the ')'
+                if (openCount > 0) {
+                    result.append(c);
+                }
             }
         }
-        for(i=0;i<strf.length();i++)
-        {
-            if(strf.charAt(i)=='(')
-            {
-                c3++;
-            }
-            else{
-                c4++;
-            }
-        }
-        if(c3==c4)
-        {
-            return strf;
-        }
-        else{
-            return ret;
-        }
+        
+        return result.toString();
     }
 }
